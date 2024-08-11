1bba08e92beff9aa3a9362cb6bf4a21a2b3bb7ff
Juliette Audema <juliette.audema@gmail.com>
2024-08-11 15:34:45 +0200

    First draft

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,17 @@ module.exports = {
   content: ['./src/index.html'],
   theme: {
     extend: {},
+    fontFamily: {
+      figtree: ["Figtree", "sans-serif"]
+    },
+    colors: {
+      'yellow': "hsl(47, 88%, 63%)",
+      'white': "hsl(0, 0%, 100%)",
+      'gray': {
+        500: "hsl(0, 0%, 42%)",
+        950: "hsl(0, 0%, 7%)",
+      },
+    },
   },
   plugins: [],
 }
